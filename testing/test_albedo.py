7894460a03fffb695cf6a91672e04b8202583fa2
thomasgoelles <thomas.goelles@gmail.com>
2022-05-04 13:47:52 +0000

    added test solution

--- testing/test_albedo.py
+++ testing/test_albedo.py
@@ -2,7 +2,9 @@ from albedo import check_for_new_snow, calc_surface_albedo
 import pytest_check as check
 
 
-# hint: use check.is_false() and check.is_true()
+# hint: use check.is_false() and check.is_true() and check.equal()
+
+
 def test_check_for_new_snow():
     pass
 
